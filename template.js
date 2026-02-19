28f0398fbb5b1d301e371da5fc387df3117486bf
Cyril-Ugolini <cyril.ugolini@hotmail.com>
2026-02-19 14:07:31 +0100

    template.html et template.js

---
+++ template.js
@@ -0,0 +1,23 @@
+fetch("template.html")
+    .then(res => res.text())
+    .then(html => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, "text/html");
+
+        // On regarde quel header la page demande
+        const headerType = document.body.getAttribute("data-header");
+
+        if (headerType === "retour") {
+            // Injecter le header avec bouton retour
+            document.getElementById("header").innerHTML =
+                doc.getElementById("tpl-header-retour").innerHTML;
+        } else {
+            // Injecter le header complet (par défaut)
+            document.getElementById("header").innerHTML =
+                doc.getElementById("tpl-header").innerHTML;
+        }
+
+        // Inject footer
+        document.getElementById("footer").innerHTML =
+            doc.getElementById("tpl-footer").innerHTML;
+    });
